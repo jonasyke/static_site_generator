5b5d866a1566878051fee46bc216b07f646fd474
jonasyke <jonasyke171@gmail.com>
2025-05-21 14:20:19 -0800

    CH3:L5 Regex nonsense

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, extract_markdown_links, extract_markdown_images
 
 
 class TestTextNode(unittest.TestCase):
@@ -35,6 +35,35 @@ class TestTextNode(unittest.TestCase):
     def test_repr_negative(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(repr(node), "TextNode(This is a text node, italic, None)")
+
+    def test_extract_markdown_links(self):
+        text = "[Boot.dev](https://www.boot.dev)"
+        expected = [("Boot.dev", "https://www.boot.dev")]
+        result = extract_markdown_links(text)
+        self.assertEqual(result, expected)
+
+    def test_extract_markdown_links_no_match(self):
+        text = "No links here!"
+        expected = []
+        result = extract_markdown_links(text)
+        self.assertEqual(result, expected)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_no_match(self):
+        matches = extract_markdown_images("This is text with no images")
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_images_empty_string(self):
+        matches = extract_markdown_images("")
+        self.assertListEqual([], matches)
+
+    
+    
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class TextType(Enum):
     TEXT = "Normal"
@@ -23,7 +24,22 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def extract_markdown_images(text):
+    """
+     takes raw markdown text and returns a list of tuples. Each tuple should contain the alt text and the URL of any markdown images.
+    """
+    image_pattern = r"!\[([^\]]+)\]\(([^)]+)\)"
+    matches = re.findall(image_pattern, text)
+    return [(alt_text, url) for alt_text, url in matches]
 
-    
-    
+def extract_markdown_links(text):
+    """
+         extracts markdown links instead of images. It should return tuples of anchor text and URLs.
+    """
+    link_pattern = r"\[([^\]]+)\]\(([^)]+)\)"
+    matches = re.findall(link_pattern, text)
+    return [(anchor_text, url) for anchor_text, url in matches]
+
+
     
