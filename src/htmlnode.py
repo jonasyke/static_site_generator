12f62fdf5e5f59a24a5ef0e868484dc1fd9e7410
jonasyke <jonasyke171@gmail.com>
2025-05-21 10:51:29 -0800

    CH2 Finished

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import TextNode, TextType
+
 class HTMLNode(object):
 
 
@@ -51,7 +53,21 @@ class ParentNode(HTMLNode):
         html += f"</{self.tag}>"
         return html
     
-    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value=None, props={"src": text_node.url})
+    else:
+        raise Exception("Unknown text type")
         
 
                
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
+from textnode import TextNode, TextType
 
 class TestHTMLNode(unittest.TestCase):
     
@@ -89,6 +90,39 @@ class TestHTMLNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "http://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props, {"href": "http://example.com"})
+
+
     
 
 
